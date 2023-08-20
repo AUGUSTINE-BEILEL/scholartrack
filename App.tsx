ca339ae28d9b1b42dedc9987733acc62b946e420
AUGUSTINE-BEILEL <beilelaugustine25@gmail.com>
2023-08-20 17:06:06 +0300

    Initialised Git Repo

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{color: 'green'}}>Scholar Track</Text>
+      <Button title='Submit'/>
+    </View>
+  )
+}
+
+export default App
